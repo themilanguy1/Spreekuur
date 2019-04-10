64e967221a0576e5aab94e367eac2ed3a37a855e
themilanguy1 <milan.gupta@ymail.com>
2019-04-10 13:33:35 +0200

    persoonlijke info bij vraag stellen pagina first draft klaar -milan

--- persoonlijke_informatie.php
+++ persoonlijke_informatie.php
@@ -1,7 +1,7 @@
 <?php
 //var_dump($_POST);
 ?>
-<html>
+<html lang="nl">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,24 +11,23 @@
 <body>
 <div class="card rounded-0">
     <div class="card-header">
-        <h3 class="mb-0">Login</h3>
-        <h5 class="mb-0">Hier bevat instrucie hoe moet je inloggen of wat is required en niet.</h5>
+        <h4 class="mb-0">Voordat u uw vraag stelt, zouden wij nog iets meer informatie willen hebben.</h4>
+        <p>Wij gebruiken deze informatie alleen maar </p>
     </div>
     <div class="card-body bg-light">
-        <form onSubmit="if(!confirm('Let op: Hoe goed wij u kunnen helpen hangt af van hoeveel informatie u ons geeft. ' +
-         'Dus als u bijvoorbeeld nog medicatie neemt of bestaande gezondheidsproblemen heeft, zouden wij dit graag willen weten.')){return false;}"
-              class="form" role="form" autocomplete="off" id="formLogin" novalidate="" method="POST">
-            <h4>Verplichte velden *</h4>
+        <form class="form" method="POST">
+            <h5>Verplichte velden *</h5>
             <div class="form-group">
-                <label for="email">E-mail *</label>
-                <input type="email" class="form-control form-control-md rounded-0" name="email" id="email" required
-                       placeholder="Voorbeeld@email.com">
+                <label for="email">E-mail (waar we ons antwoord naar toe zullen sturen)*</label>
+                <input type="email" class="form-control form-control-md rounded-0" name="email" id="email"
+                       placeholder="Voorbeeld@email.com" required>
+                <br>
                 <label>Geslacht * </label>
                 <input type="radio" name="geslacht" value="M" required> Man
-                <input type="radio" name="geslacht" value="V" required> Vrouw
+                <input type="radio" name="geslacht" value="V"> Vrouw
                 <br>
                 <label>Leeftijdsgroep *</label>
-                <select name="leeftijd">
+                <select name="leeftijd" required>
                     <option value="0-15">0-15</option>
                     <option value="16-25">16-25</option>
                     <option value="26-35">26-35</option>
@@ -39,36 +38,28 @@
                     <option value="76-85">76-85</option>
                     <option value="86+">86+</option>
                 </select>
+                <button type="submit" class="btn btn-success btn-lg float-right" id="btnLogin">Verder
+                </button>
             </div>
 
             <br>
-            <h4>Optionele Velden </h4>
+            <br>
+            <br>
+            <h5>Optionele Velden </h5>
+            <p></p>
             <div class="form-group">
-                <label>Patientnummer</label>
-                <input type="number" class="form-control form-control-md rounded-0" id="patientnummer"
-                       name="patientnummer"
-                       placeholder="12345678">
                 <label>Voornaam</label>
                 <input type="text" class="form-control form-control-md rounded-0" id="voornaam" name="voornaam"
                        placeholder="Kees">
                 <label>Achternaam</label>
                 <input type="text" class="form-control form-control-md rounded-0" id="achternaam" name="achternaam"
                        placeholder="Janssen">
-                <label>Telefoon</label>
+                <label>Telefoon Nummer (een andere manier om met u contact op te nemen.</label>
                 <input type="text" class="form-control form-control-md rounded-0" id="telefoon" name="telefoon"
-                       placeholder="Type your password.">
-                <label>Woonplaats</label>
-                <input type="text" class="form-control form-control-md rounded-0" id="woonplaats" name="woonplaats"
-                       placeholder="Plaatsnaam">
-                <label>Postcode</label>
-                <input type="text" class="form-control form-control-md rounded-0" id="postcode" name="poscode"
-                       placeholder="3021 XT">
-                <label>Adres/huisnummer</label>
-                <input type="text" class="form-control form-control-md rounded-0" id="adres" name="adres"
-                       placeholder="Straat 21">
+                       placeholder="0612345678">
                 <label>Medicatie</label>
                 <textarea class="form-control form-control-md rounded-0" id="medicatie" name="medicatie"
-                          placeholder="Bloedverdunners"></textarea>
+                          placeholder="Bloedverdunnerde medicatie, etc."></textarea>
                 <label>Gezondheidsklachten</label>
                 <textarea class="form-control form-control-md rounded-0" id="gezondheids_klachten"
                           name="gezondheids_klachten"
@@ -78,7 +69,9 @@
                           name="aanvullende_informatie"
                           placeholder="allergie/roken/drinken"></textarea>
             </div>
-            <button type="submit" name="submit" class="btn btn-success btn-lg float-right" id="btnLogin">Inloggen
+            <button type="button" class="btn btn-primary btn-lg float-left" >Terug
+            </button>
+            <button type="submit" name="submit" class="btn btn-success btn-lg float-right" id="btnLogin">Verder
             </button>
         </form>
     </div>
