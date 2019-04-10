c79382ee7214d8f4fec16007653fa5600732f1fe
Akahama <orel971111@gmail.com>
2019-04-10 13:34:53 +0200

    Homepage version 1.0 gemaakt

--- home.php
+++ home.php
@@ -7,6 +7,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="style/bootstrap.css">
 </head>
+<script type="text/javascript" src="script/font-awesome/font-awesome.js"></script>
 <body>
+<div class="container">
+    <a href="feedback.php">
+        <img src="img/feedback.jpg" alt="Geen afbeelding gevonden" class="rounded mx-auto d-block"
+             style="width:200px;height:200px;">
+    </a>
+    <br>
+    <a href="vragen.php">
+        <img src="img/vragen.jpg" alt="Geen afbeelding gevonden" class="rounded mx-auto d-block"
+             style="width:200px;height:200px;">
+    </a>
+    <br>
+    <a href="inbox.php">
+        <img src="img/mail.png" alt="Geen afbeelding gevonden" class="rounded mx-auto d-block"
+             style="width:200px;height:200px;">
+    </a>
+    <a href="help.php" class="btn btn-primary btn-lg float-left " id="btnHelp"><i class="far fa-question-circle"></i>Help</a>
+    <a href="login.php" class="btn btn-primary btn-lg float-right " id="btnLogin"><i class="fas fa-sign-in-alt"></i>Login</a>
+</div>
 </body>
 </html>
