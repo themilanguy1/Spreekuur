9b7a1f4fa6d24d171e4753d9563d833d68dcf10f
Akahama <orel971111@gmail.com>
2019-04-10 15:04:45 +0200

    Beginnen met vragen page te maken.

--- vragen.php
+++ vragen.php
@@ -1,1 +1,30 @@
 <?php
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style/bootstrap.css">
+</head>
+<script type="text/javascript" src="script/font-awesome/font-awesome.js"></script>
+<body>
+<div class="container">
+    <a href="feedback.php">
+        <img src="img/feedback.jpg" alt="Geen afbeelding gevonden" class="rounded mx-auto d-block"
+             style="width:200px;height:200px;">
+    </a>
+    <br>
+    <a href="vragen.php">
+        <img src="img/vragen.jpg" alt="Geen afbeelding gevonden" class="rounded mx-auto d-block"
+             style="width:200px;height:200px;">
+    </a>
+    <br>
+    <a href="inbox.php">
+        <img src="img/mail.png" alt="Geen afbeelding gevonden" class="rounded mx-auto d-block"
+             style="width:200px;height:200px;">
+    </a>
+    <a href="home.php" class="btn btn-primary btn-lg float-left " id="btnHome"><i class="fas fa-undo"></i> Terug</a>
+</div>
+</body>
+<html>
