4dc75d124cd750651304c0dc6494c11aed0b3dd3
Akahama-Patryk <orel971111@gmail.com>
2019-04-14 21:52:37 +0200

    feedback_geven.php FRONTEND DONE style.css gemaakt voor feedback_geven To be continued...
    Signed-off-by: Akahama-Patryk <orel971111@gmail.com>

--- feedback/feedback_geven.php
+++ feedback/feedback_geven.php
@@ -1,6 +1,12 @@
 <?php
 require_once('../classes/Autoloader.php');
 Session::start();
+
+if (isset($_POST['submit'])) {
+    var_dump($_POST);
+} else {
+    echo "fuck off";
+}
 ?>
 
 <html>
@@ -9,6 +15,7 @@ Session::start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="../style/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
 </head>
 <script type="text/javascript" src="../script/font-awesome/font-awesome.js"></script>
 <body>
@@ -22,9 +29,50 @@ if (Session::feedbackPersonalDataStatus()) {
 }
 ?>
 <div class="container-fluid">
+    <h1>Feedback</h1>
+    <form method="post">
+        <div class="row">
+            <label>Uw reactie : </label>
+            <div class="col-xs-4">
+                <input type="radio" name="reaction" value="1" id="iets-1" required>
+                <label for="iets-1">
+                    <img src="../img/bad.png" alt="Geen afbeelding gevonden" class="rounded float-left mx-5 d-block">
+                </label>
+            </div>
+            <div class="col-xs-4">
+                <input type="radio" name="reaction" value="2" id="iets-2" required>
+                <label for="iets-2">
+                    <img src="../img/ok.png" alt="Geen afbeelding gevonden" class="rounded float-left mx-5 d-block">
+                </label>
+            </div>
+            <div class="col-xs-4">
+                <input type="radio" name="reaction" value="1" id="iets-3" required>
+                <label for="iets-3">
+                    <img src="../img/good.png" alt="Geen afbeelding gevonden" class="rounded float-left mx-5 d-block">
+                </label>
+            </div>
+        </div>
+        <div class="form-group">
+            <br>
+            <label>Uw zorginstelling : </label>
+            <select name="feedback_zorg">
+                <option value="zorginstelling1">Zorginstellling 1</option>
+                <option value="zorginstelling2">Zorginstellling 2</option>
+                <option value="zorginstelling3">Zorginstellling 3</option>
+            </select>
+            </br></br>
+            <label for="feedback_textarea">Uw feedback : </label>
+            <textarea name="feedback_form_text" class="form-control" rows="10"></textarea>
+        </div>
+        <button type="submit" name="submit" class="btn btn-success btn-lg float-right" id="btnSend"><i
+                    class="far fa-share-square"></i>Versturen
+        </button>
+    </form>
+    <br>
     <div class="col-16 align-bottom col-lg">
-        <a href="../help.php" class="btn btn-primary btn-lg float-left" id="btnHelp"><i class="far fa-question-circle"></i>&nbspHelp</a>
-        <a href="../home.php" class="btn btn-primary btn-lg float-right" id="btnBack"><i class="fas fa-undo"></i>&nbspTerug</a>
+        <a href="../help.php" class="btn btn-primary btn-lg float-left " id="btnHelp"><i
+                    class="far fa-question-circle"></i>&nbspHelp</a>
+        <a href="../home.php" class="btn btn-primary btn-lg float-left mx-2" id="btnBack"><i class="fas fa-undo"></i>&nbspTerug</a>
     </div>
 </div>
 </body>
